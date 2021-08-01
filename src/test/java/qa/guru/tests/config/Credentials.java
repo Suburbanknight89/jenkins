97a63057ca1a757735bee26a68b6b0d8dca26ba9
suburbanknight89 <lumenteam@gmail.com>
2021-08-01 20:42:20 +0300

    add 1 congig and url properties

---
+++ src/test/java/qa/guru/tests/config/Credentials.java
@@ -0,0 +1,8 @@
+package qa.guru.tests.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Credentials {
+
+	public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+}
---
+++ src/test/java/qa/guru/tests/properties/OwnerTests.java
@@ -0,0 +1,24 @@
+package qa.guru.tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+import static qa.guru.tests.config.Credentials.credentials;
+
+@Tag("properties")
+public class OwnerTests {
+
+	@Test
+	void readCredentialsTest() {
+		String login = credentials.login();
+		String password = credentials.password();
+
+		System.out.println(login);
+		System.out.println(password);
+
+		String message = format("I try to login as %s with password %s", login, password);
+
+		System.out.println(message);
+	}
+}
